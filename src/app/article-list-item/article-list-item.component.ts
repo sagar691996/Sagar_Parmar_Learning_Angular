fb31c3cd27bfd288c09396d9dc8af1a34a08d2ea
sagar691996 <w0857561@myscc.ca>
2024-09-25 16:38:20 -0400

    created 2 components in app folder

---
+++ src/app/article-list-item/article-list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './article-list-item.component.html',
+  styleUrl: './article-list-item.component.css'
+})
+export class ArticleListItemComponent {
+
+}
