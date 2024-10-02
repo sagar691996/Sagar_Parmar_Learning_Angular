4547a58866bf3983c4f716152a51047bd487c791
sagar691996 <w0857561@myscc.ca>
2024-10-02 17:58:33 -0400

    Complete

--- src/app/flags-list-item/flags-list-item.component.ts
+++ src/app/flags-list-item/flags-list-item.component.ts
@@ -1,21 +1,26 @@
 import {Component, Input} from '@angular/core';
 import {Flags} from "../Shared/Modules/flags";
 import {NgIf} from "@angular/common";
+import { NgStyle } from '@angular/common';
 
 
 @Component({
   selector: 'app-flags-item',
   standalone: true,
   imports: [
-    NgIf
+    NgIf, NgStyle
   ],
   templateUrl: './flags-list-item.component.html',
   styleUrl: './flags-list-item.component.css'
 })
 export class FlagsListItemComponent {
+toggleInStockStatus() {
+throw new Error('Method not implemented.');
+}
   //Input marks the property as an input property
 //that can receive data from a parent
   //? says it is optional
   @Input() flag?: Flags;
+isInStock: any;
 
 }
--- src/app/flags-list/flags-list.component.ts
+++ src/app/flags-list/flags-list.component.ts
@@ -1,40 +1,38 @@
-import { Component } from '@angular/core';
-import {NgForOf} from "@angular/common";
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NgForOf } from '@angular/common';
 import { Flags } from '../Shared/Modules/flags';
-import {FlagsListItemComponent} from '../flags-list-item/flags-list-item.component';
-
+import { FlagsListItemComponent } from '../flags-list-item/flags-list-item.component';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-flags-list',
   standalone: true,
-  imports: [NgForOf, NgForOf, FlagsListItemComponent],
+  imports: [NgForOf, FlagsListItemComponent, NgStyle],
   templateUrl: './flags-list.component.html',
-  styleUrl: './flags-list.component.css'
+  styleUrls: ['./flags-list.component.css'],
+  schemas: [NO_ERRORS_SCHEMA] // Optionally suppresses schema validation
 })
-
 export class FlagsListComponent {
-
+  // Rest of the component code...
   title: string = 'National Flags';
 
-  displayedColumns:string[]= ['id', 'country', 'material', 'size', 'isInStock'];
-  flagList : Flags[] = [
-    { id: 1 , country: "India", material: "Paper", size: "1.5 meter", isInStock: true },
-    { id: 2 , country: "Canada", material: "Plastic", size: "1.9 meter", isInStock: false },
-    { id: 3 , country: "U.S.A", material: "Paper", size: "1.0 meter", isInStock: true },
-    { id: 4 , country: "U.K", material: "Plastic", size: "1.7 meter", isInStock: false },
-    { id: 5 , country: "Japan", material: "Paper", size: "1.2 meter", isInStock: true },
-
-  ];
-    //Catch the onclick event from the html
-    selectedFlag?: Flags;
-    // //function to set which student to display
-    selectFlag(flag: Flags): void {
-      this.selectedFlag = flag;
-    }
-
-    toggleInStockStatus(flags: Flags): void {
-      flags.isInStock = !flags.isInStock;
-    }
+  displayedColumns: string[] = ['id', 'country', 'material', 'size', 'isInStock'];
   
+  flagList: Flags[] = [
+    { id: 1, country: 'India', material: 'Paper', size: '1.5 meter', isInStock: true },
+    { id: 2, country: 'Canada', material: 'Plastic', size: '1.9 meter', isInStock: false },
+    { id: 3, country: 'U.S.A', material: 'Paper', size: '1.0 meter', isInStock: true },
+    { id: 4, country: 'U.K', material: 'Plastic', size: '1.7 meter', isInStock: false },
+    { id: 5, country: 'Japan', material: 'Paper', size: '1.2 meter', isInStock: true },
+  ];
 
+  selectedFlag?: Flags;
+
+  selectFlag(flag: Flags): void {
+    this.selectedFlag = flag;
+  }
+
+  toggleInStockStatus(flag: Flags): void {
+    flag.isInStock = !flag.isInStock;
+  }
 }
