25e3d34c9a23d34bce76d2b01d875465a5346156
sagar691996 <w0857561@gmail.com>
2024-11-01 18:49:24 -0400

    Complete step 2

--- src/app/services/country-flag.service.ts
+++ src/app/services/country-flag.service.ts
@@ -8,14 +8,6 @@ import { mockFlag } from '../Shared/mock-flag.data';
 })
 export class CountryFlagService {
 
-  // private flags: Flag[] = [
-  //   { id: 1, country: 'India', material: 'Paper', size: '1.5 meter', isInStock: true },
-  //   { id: 2, country: 'Canada', material: 'Plastic', size: '1.9 meter', isInStock: false },
-  //   { id: 3, country: 'U.S.A', material: 'Paper', size: '1.0 meter', isInStock: true },
-  //   { id: 4, country: 'U.K', material: 'Plastic', size: '1.7 meter', isInStock: false },
-  //   { id: 5, country: 'Japan', material: 'Paper', size: '1.2 meter', isInStock: true }
-  // ];
-
   //local copy of flag list
   private local_flagList:Flag[] = mockFlag;
   constructor(){}
