50380326de832595f41d8f523d016e8dc7042762
sagar691996 <w0857561@myscc.ca>
2024-11-22 21:38:43 -0500

    Implemented HoverHighlight directive with customizable input

--- src/app/flag-list/flag-list.component.ts
+++ src/app/flag-list/flag-list.component.ts
@@ -5,11 +5,12 @@ import { Flag } from '../Shared/Models/flag';
 import { CountryFlagService } from '../services/country-flag.service';
 import {ActivatedRoute, Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { TitleNamePipe } from "../pipes/title-name.pipe";
+import {HoverHighlightDirective} from "../directives/hover-highlight.directive";
 
 @Component({
   selector: 'app-flag-list',
   standalone: true,
-  imports: [NgForOf, RouterLink, NgOptimizedImage, TitleNamePipe],
+  imports: [NgForOf, RouterLink, NgOptimizedImage, TitleNamePipe, HoverHighlightDirective],
   templateUrl: './flag-list.component.html',
   styleUrl: './flag-list.component.scss'
 })
